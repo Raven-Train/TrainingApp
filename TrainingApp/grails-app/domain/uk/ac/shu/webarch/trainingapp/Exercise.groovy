7a43c9724b64fbaa2ffed2ffcbc5cd9a9e699acc
GeorgeUni <b1037178@my.shu.ac.uk>
2014-03-11 16:52:04 +0000

    Added attributes for Exercise class and partial for sessionExercise

--- TrainingApp/grails-app/domain/uk/ac/shu/webarch/trainingapp/Exercise.groovy
+++ TrainingApp/grails-app/domain/uk/ac/shu/webarch/trainingapp/Exercise.groovy
@@ -1,7 +1,15 @@
 package uk.ac.shu.webarch.trainingapp
 
 class Exercise {
-
+	string exerciseName
+	int percentage
+	int improvement
+	int goalWeight
+	string bodyPart
+	string equipment
+	
     static constraints = {
+	
+	
     }
 }
--- TrainingApp/grails-app/domain/uk/ac/shu/webarch/trainingapp/Sessionexercise.groovy
+++ TrainingApp/grails-app/domain/uk/ac/shu/webarch/trainingapp/Sessionexercise.groovy
@@ -1,7 +1,10 @@
 package uk.ac.shu.webarch.trainingapp
 
 class Sessionexercise {
+	int reps
+	int weights
 
     static constraints = {
     }
 }
+//leaving some datatypes alone until we figure out keys and foreign keys
